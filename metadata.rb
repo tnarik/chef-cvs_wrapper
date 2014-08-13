ccf6d1d38e5f30eb294c3a26989d3042af3242be
tnarik <tnarik@gmail.com>
2014-08-13 08:07:34 +0100

    Got rid of version specific depedency for 'hostsfile' cookbook. Used rubygems version of 'hostsfile' gem.

--- metadata.rb
+++ metadata.rb
@@ -7,8 +7,6 @@ long_description 'Installs/Configures cvs_wrapper'
 version          '0.1.0'
 
 #ssh_known_hosts / ssh_user / ssh
-%w{sudo tar ruby_install ssh-util}.each do |cookbook|
+%w{hostsfile sudo tar ruby_install ssh-util}.each do |cookbook|
   depends cookbook
 end
-
-depends 'hostsfile', '>= 2.0.1'
--- recipes/dependencies.rb
+++ recipes/dependencies.rb
@@ -11,12 +11,6 @@ gem_package 'thecon' do
   gem_binary File.join(node[:system_ruby][:bin_path], "gem")
 end
 
-cookbook_file "hostsfile_gem_file" do
-  path "/tmp/hostsfile-0.0.1.gem"
-  action :create_if_missing
-end
-
 gem_package 'hostsfile' do
   gem_binary File.join(node[:system_ruby][:bin_path], "gem")
-  source "/tmp/hostsfile-0.0.1.gem"
 end
