472141aa0e261cd05ca5ef40038535880b7ab236
tnarik <tnarik@gmail.com>
2015-02-20 12:17:43 +0000

    merged changes from backups

--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -1,3 +1,11 @@
 require 'serverspec'
 
 set :backend, :exec
+
+describe package('cvs') do
+  it { should be_installed }
+end
+
+describe command('which cvs') do
+  its(:exit_status) { should eq 0 }
+end
